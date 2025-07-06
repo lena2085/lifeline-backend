e20d1453eb5d5923941c168ba5f9aa6ea8615abd
Lena <lenaelizabeth2085@gmail.com>
2025-07-06 22:12:35 +0530

    first commit

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use('/api', require('./routes/api'));
 
